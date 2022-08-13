e70ce9fbdfa1d1d0f0be65f15d6e97178e781d19
Stephanie Smithson <stephaniesmithson16@gmail.com>
2022-08-13 14:46:42 -0500

    Initial Commit

---
+++ github-connect/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  github-connect
+//
+//  Created by Stephanie Smithson on 8/13/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ github-connect/github_connectApp.swift
@@ -0,0 +1,17 @@
+//
+//  github_connectApp.swift
+//  github-connect
+//
+//  Created by Stephanie Smithson on 8/13/22.
+//
+
+import SwiftUI
+
+@main
+struct github_connectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
