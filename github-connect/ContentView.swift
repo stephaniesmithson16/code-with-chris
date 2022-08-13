d2e7739bdb4dc6efb612b8a068a974a9a5e2c0de
Stephanie Smithson <stephaniesmithson16@gmail.com>
2022-08-13 14:49:41 -0500

    Added even more

--- github-connect/ContentView.swift
+++ github-connect/ContentView.swift
@@ -9,8 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, code crew!")
-            .padding()
+        VStack{
+            Text("Hello, code crew!")
+                .padding()
+            Text("Stephanie")
+        }
     }
 }
 
